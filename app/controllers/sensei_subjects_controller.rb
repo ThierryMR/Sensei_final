d248b80d8e65bcf8772ed52ddc85469397508146
Henrique Aguilar <henriaguilar@gmail.com>
2018-11-21 16:29:00 -0200

    render index in sensei show and user show is working

--- app/controllers/sensei_subjects_controller.rb
+++ app/controllers/sensei_subjects_controller.rb
@@ -11,7 +11,6 @@ class SenseiSubjectsController < ApplicationController
     @sensei_subject = SenseiSubject.new
     @sensei_subject.sensei = @sensei
     @sensei_subject.price_per_hour = params[:sensei_subject][:price_per_hour].to_i
-    raise
     @sensei_subject.subject = Subject.find(params[:sensei_subject][:subject_id])
     if @sensei_subject.save
       sensei_sensei_subjects_path(@sensei)
