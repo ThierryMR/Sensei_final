bdd2c88225555ec5af9624dd035cdceb23d25df4
Thierry Silvagnoli <thierry.martins.r.s@gmail.com>
2018-11-21 20:28:31 -0300

    Letreiro com problemas no segundo, e about page aprimorada

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -1,23 +1,36 @@
 import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
+
   new Typed('#banner-typed-text', {
     strings: ["Matemática", "Inglês", "Português", "Espanhol", "Biologia", "Química", "Física", "Geografia", "História"],
     typeSpeed: 35,
     loop: true
   });
-}
 
 
-
-
-function loadDynamicBannerText2() {
-  new Typed('#banner-typed-galera', {
+   new Typed('#banner-typed-galera', {
     strings: ["Conheça a galera", "Passe o mouse"],
     typeSpeed: 50,
     loop: true
 
   });
+
+
 }
 
-export { loadDynamicBannerText2 , loadDynamicBannerText };
+
+
+
+// function loadDynamicBannerText2() {
+//   new Typed('#banner-typed-galera', {
+//     strings: ["Conheça a galera", "Passe o mouse"],
+//     typeSpeed: 50,
+//     loop: true
+
+//   });
+// }
+export {loadDynamicBannerText };
+
+
+// export { loadDynamicBannerText2 , loadDynamicBannerText };
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,12 @@ import "bootstrap";
 import { loadDynamicBannerText } from '../components/banner';
 loadDynamicBannerText() ;
 
+// import { loadDynamicBannerText2,  } from '../components/banner';
+// loadDynamicBannerText2();
+
+
 
 // So funciona a primeira importacao damnt
 
-import { loadDynamicBannerText2 } from '../components/banner';
-loadDynamicBannerText2();
 
 
