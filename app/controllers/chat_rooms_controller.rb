e7a87b7d536967dec1b858185ba29c17d8f1b7bb
Davis JR <davisjr2000@hotmail.com>
2018-11-21 22:52:51 -0200

    Reviews

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -7,7 +7,7 @@ class ChatRoomsController < ApplicationController
   def new
     @chat_room = ChatRoom.new
     @chat_room.lesson_request_id = params[:format]
-    @chat_room.name = "Teste"
+    @chat_room.name = "Aula com #{@chat_room.lesson_request.sensei.user.username}"
     @chat_room.save
     redirect_to @chat_room
   end
@@ -15,5 +15,9 @@ class ChatRoomsController < ApplicationController
   def rating
     @chat_room = ChatRoom.find(params[:chat_room_id])
     @lesson_request = @chat_room.lesson_request
+    # if @lesson_request.save && @lesson_request.rating.nil? == false && current_user.id == @lesson_request.user.id
+    #   @lesson_request.sensei.avg_rating << @lesson_request.rating
+    #   @lesson_request.sensei.save
+    # end
   end
 end
